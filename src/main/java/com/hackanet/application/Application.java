66d85616b21d42ec0bea81a11cbb8bec4464f09d
IskanderValiev <iskand.valiev@yandex.ru>
2019-10-18 20:52:22 +0300

    added main and project structure

---
+++ src/main/java/com/hackanet/application/Application.java
@@ -0,0 +1,16 @@
+package com.hackanet.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Iskander Valiev
+ * created by isko
+ * on 10/18/19
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
