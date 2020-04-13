4d5c69117a234770511c5a24e3faaaf3b13eb4e8
IskanderValiev <iskand.valiev@yandex.ru>
2020-04-13 10:01:33 +0300

    unrestricted cross origin limits

--- src/main/java/com/hackanet/security/filters/CORSFilter.java
+++ src/main/java/com/hackanet/security/filters/CORSFilter.java
@@ -28,7 +28,7 @@ public class CORSFilter implements Filter {
 
             // Access-Control-Allow-Origin
             String origin = request.getHeader("Origin");
-            response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+            response.setHeader("Access-Control-Allow-Origin", "true");
             response.setHeader("Vary", "Origin");
 
             // Access-Control-Max-Age
